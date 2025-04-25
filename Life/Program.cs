09d976efd254bd5f51617736ae6f35a536ef17dc
samoh <samoh@HUSKYEG>
2025-04-25 21:12:49 +0300

    2 task

--- Life/Program.cs
+++ Life/Program.cs
@@ -9,6 +9,12 @@ using System.Text.Json;
 using Newtonsoft.Json;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.IO;
+using System.Text.Json;
+using ScottPlot;
+using ScottPlot.Palettes;
+using ScottPlot.MultiplotLayouts;
+using ScottPlot.PlotStyles;
 
 namespace cli_life
 {
@@ -34,6 +40,7 @@ namespace cli_life
     {
         public readonly Cell[,] Cells;
         public readonly int CellSize;
+        public double LiveDensity_;
 
         public int Columns { get { return Cells.GetLength(0); } }
         public int Rows { get { return Cells.GetLength(1); } }
@@ -42,6 +49,7 @@ namespace cli_life
 
         public Board(int width, int height, int cellSize, double liveDensity = .5)
         {
+            LiveDensity_ = liveDensity;
             CellSize = cellSize;
 
             Cells = new Cell[width / cellSize, height / cellSize];
@@ -55,6 +63,7 @@ namespace cli_life
 
         public Board(GameSettings settings)
         {
+            LiveDensity_ = settings.LiveDensity;
             CellSize = settings.CellSize;
 
             Cells = new Cell[settings.Width / settings.CellSize, settings.Height / settings.CellSize];
@@ -130,7 +139,7 @@ namespace cli_life
             int yLen = Cells.Length / Cells.GetLength(0);
             int xLen = Cells.GetLength(0);
             string[] lines = File.ReadAllLines(fileName);
-            for (int y = 0; y < Math.Min(lines.Length, yLen) ; y++)
+            for (int y = 0; y < Math.Min(lines.Length, yLen); y++)
             {
                 for (int x = 0; x < Math.Min(lines[y].Length, xLen); x++)
                 {
@@ -314,13 +323,224 @@ namespace cli_life
 
             return history.Distinct().Count() == 1 && history.Count == StabilityThreshold;
         }
+        public void SaveValue(int generation, string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                List<string> lines = File.ReadAllLines(fileName).ToList();
+                string newRecord = $"{DateTime.Now.ToString()}: {generation}";
+                if (lines.Count > 0)
+                    lines[lines.Count - 1] = newRecord;
+                else
+                    lines.Add(newRecord);
+                int average = 0;
+                foreach (string line in lines)
+                {
+                    average += int.Parse(line.Split(": ")[1]);
+                }
+                average /= lines.Count;
+                lines.Add($"Среднее число поколений: {average}");
+                File.WriteAllLines(fileName, lines);
+            }
+            else
+            {
+                throw new Exception($"File {fileName} not found");
+            }
+        }
     }
+
+    public class Statistics
+    {
+        public static void CalcAndGet()
+        {
+            string projDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string allDataPath = Path.Combine(projDir, "dataAll.txt");;
+            string allPlotPath = Path.Combine(projDir, "plotAll.png");
+
+            RunPopulationAnalysisAll(allDataPath);
+            PlotForAll(allDataPath, allPlotPath);
+        }
+
+        private static void RunPopulationAnalysis(string fileName)
+        {
+            double density = 0.4;
+            int maxGenerations = 600;
+            StabilityAnalyzer stabilityAnalyzer = new StabilityAnalyzer();
+
+            File.WriteAllText(fileName,
+                "Поколение Живых_клеток\n");
+
+            var board = new Board(50, 20, 1, density);
+            int gen = 0;
+            while (gen < maxGenerations && !stabilityAnalyzer.CheckStability(board))
+            {
+                int aliveCount = 0;
+                for (int y = 0; y < board.Rows; y++)
+                    for (int x = 0; x < board.Columns; x++)
+                        if (board.Cells[x, y].IsAlive)
+                            aliveCount++;
+
+                File.AppendAllText(fileName,
+                    $"{gen} {aliveCount}\n");
+
+                board.Advance();
+                gen++;
+            }
+        }
+        private static void RunPopulationAnalysisAll(string fileName)
+        {
+            double[] densities = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 };
+            int maxGenerations = 600;
+
+            File.WriteAllText(fileName,
+                "Поколение Плотность Живых_клеток\n");
+
+            foreach (var density in densities)
+            {
+                var board = new Board(50, 20, 1, density);
+
+                for (int gen = 0; gen < maxGenerations; gen++)
+                {
+                    int aliveCount = 0;
+                    for (int y = 0; y < board.Rows; y++)
+                        for (int x = 0; x < board.Columns; x++)
+                            if (board.Cells[x, y].IsAlive)
+                                aliveCount++;
+
+                    File.AppendAllText(fileName,
+                        $"{gen} {density} {aliveCount}\n");
+
+                    board.Advance();
+                }
+            }
+        }
+
+        public static void CreatePlot(string dataFileName, string plotFileName)
+        {
+            var data = File.ReadAllLines(dataFileName)
+                .Skip(1)
+                .Select(line => line.Split(' '))
+                .Where(parts => parts.Length == 2)
+                .Select(parts => new
+                {
+                    Generation = int.Parse(parts[0]),
+                    AliveCells = int.Parse(parts[1])
+                })
+                .ToList();
+
+            var plot = new Plot();
+
+            var xValues = data.Select(x => (double)x.Generation).ToArray();
+            var yValues = data.Select(x => (double)x.AliveCells).ToArray();
+            var sig = plot.Add.Scatter(xValues, yValues);
+            sig.Color = Colors.Red;
+            sig.MarkerSize = 1;
+
+            plot.Title("Динамика числа живых клеток плотность 0.4", size: 16);
+            plot.XLabel("Номер поколения", size: 14);
+            plot.YLabel("Число клеток", size: 14);
+
+            plot.Axes.AutoScale();
+
+            plot.SavePng(plotFileName, 500, 500);
+        }
+
+        public static void PlotForAll(string dataFileName, string plotFileName)
+        {
+            var data = File.ReadAllLines(dataFileName)
+                .Skip(1)
+                .Select(line => line.Split(' '))
+                .Where(parts => parts.Length == 3)
+                .Select(parts => new
+                {
+                    Generation = int.Parse(parts[0]),
+                    Density = double.Parse(parts[1]),
+                    AliveCells = int.Parse(parts[2])
+                })
+                .ToList();
+
+            var plot = new Plot();
+
+            var groups = data.GroupBy(x => x.Density);
+
+            var colors = new[] {
+        Colors.Red,
+        Colors.Orange,
+        Colors.Yellow,
+        Colors.Green,
+        Colors.SkyBlue,
+        Colors.DarkBlue,
+        Colors.Violet,
+        Colors.Gray,
+        Colors.Black
+      };
+
+            int colorIndex = 0;
+            foreach (var group in groups)
+            {
+                var xValues = group.Select(x => (double)x.Generation).ToArray();
+                var yValues = group.Select(x => (double)x.AliveCells).ToArray();
+
+                var sig = plot.Add.Scatter(xValues, yValues);
+                sig.Color = colors[colorIndex];
+                sig.MarkerSize = 1;
+                sig.LegendText = group.First().Density.ToString();
+
+                colorIndex = (colorIndex + 1) % colors.Length;
+            }
+
+            plot.Title("Динамика числа живых клеток", size: 16);
+            plot.XLabel("Номер поколения", size: 14);
+            plot.YLabel("Число клеток", size: 14);
+
+            plot.Axes.AutoScale();
+            plot.ShowLegend();
+
+            plot.SavePng(plotFileName, 2000, 1000);
+        }
+        public static void CalcAvgStability()
+        {
+            StabilityAnalyzer analyzer = new StabilityAnalyzer();
+            string projPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string statPath = Path.Combine(projPath, "avg_stability/");
+            string patterns = Path.Combine(projPath, "patterns/");
+            double[] densities = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 };
+            foreach (double dens in densities)
+            {
+                string filename = Path.Combine(statPath, "0_" + ((int)(dens * 10) % 10).ToString() + ".txt");
+                var f = File.Create(filename);
+                f.Close();
+                for (int i = 0; i < 15; ++i)
+                {
+                    Board board = new Board(20, 20, 1, dens);
+                    int gen = 0;
+                    while (true)
+                    {
+                        if (analyzer.CheckStability(board))
+                        {
+                            analyzer.SaveValue(gen, filename);
+                            break;
+                        }
+                        else
+                            gen++;
+                        board.Advance();
+                    }
+
+                }
+
+                Console.WriteLine("Посчитана статистика для плотности:" + dens.ToString());
+            }
+        }
+    }
+
 
     class Program
     {
         static Board board;
         static private int Delay;
         static StabilityAnalyzer stabilityAnalyzer = new StabilityAnalyzer();
+        static int generation = 1;
+        static int stableGeneration = 1;
 
         static private Board LoadFromFile(string filename)
         {
@@ -350,9 +570,12 @@ namespace cli_life
             {
                 string json = r.ReadToEnd();
                 GameSettings settings = JsonConvert.DeserializeObject<GameSettings>(json);
-                if (newRunning) {
+                if (newRunning)
+                {
                     board = new Board(settings);
-                } else {
+                }
+                else
+                {
                     board = LoadFromFile(loadPath);
                 }
                 Delay = settings.Delay;
@@ -362,7 +585,7 @@ namespace cli_life
         {
             for (int row = 0; row < board.Rows; row++)
             {
-                for (int col = 0; col < board.Columns; col++)   
+                for (int col = 0; col < board.Columns; col++)
                 {
                     var cell = board.Cells[col, row];
                     if (cell.IsAlive)
@@ -376,6 +599,26 @@ namespace cli_life
                 }
                 Console.Write('\n');
             }
+            Console.Write($"Поколение {generation}");
+            generation++;
+        }
+
+        static private int RenderCnt()
+        {
+            int ans = 0;
+            for (int row = 0; row < board.Rows; row++)
+            {
+                for (int col = 0; col < board.Columns; col++)
+                {
+                    var cell = board.Cells[col, row];
+                    if (cell.IsAlive)
+                    {
+                        ans++;
+                    }
+                }
+            }
+            generation++;
+            return ans;
         }
 
         static void printClustersInfo(string patternsPath)
@@ -387,34 +630,66 @@ namespace cli_life
                 Console.WriteLine($"{ClusterAnalyzer.ClassifyCluster(cluster, patternsPath)} (размер: {cluster.Count})");
             }
         }
-        static void Main(string[] args)
+
+        static void addInf(string path)
+        {
+            // Открываем файл для добавления
+            using (FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.Write(generation);
+                    writer.Write(" ");
+                    writer.Write(board.LiveDensity_);
+                    writer.Write(" ");
+                    writer.Write(RenderCnt());
+                    writer.Write('\n');
+                }
+            }
+        }
+
+        static void Start()
         {
             string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
             string configPath = Path.Combine(projectDirectory, "config.json");
             string filePath = Path.Combine(projectDirectory, "board.txt");
             string patternPath = Path.Combine(projectDirectory, "patterns");
+            string analiticsPath = Path.Combine(projectDirectory, "avg_stability/stableGFor0_9.txt");
+            string path = Path.Combine(projectDirectory, "dataForAll.txt");
             Reset(configPath, filePath, true);
 
             board.LoadPattern(patternPath + "/train.txt");
 
-            int generation = 0;
-            while(++generation < 0|| true)
+            while (generation < 100)
             {
                 Console.Clear();
-                Render();
+                //Render();
+                addInf(path);
 
-                if (stabilityAnalyzer.CheckStability(board))
+
+                /*if (stabilityAnalyzer.CheckStability(board))
                 {
-                    Console.WriteLine($"\nСистема стабилизировалась на поколении {generation}");
+                    Console.WriteLine($"\nНайдено стационарное состояние: {stableGeneration}");
+                    stabilityAnalyzer.SaveValue(stableGeneration, analiticsPath);
+                    printClustersInfo(patternPath);
                     break;
                 }
+                else
+                    stableGeneration++;*/
+
 
                 board.Advance();
-                Thread.Sleep(Delay);
+                //Thread.Sleep(Delay);
             }
 
-            board.SaveToFile(filePath);
-            printClustersInfo(patternPath);
+            //board.SaveToFile(filePath);
+            //printClustersInfo(patternPath);
         }
+        static void Main(string[] args)
+        {
+            //Start();
+            Statistics.CalcAndGet();
+        }
+
     }
 }
